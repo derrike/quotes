e4a552c934e158ae5b0fd24e5113356423735966
Derrike <derrike.nunn@gmail.com>
2020-11-09 12:29:14 -0600

    test with sleep

--- terraform/test/test.tf
+++ terraform/test/test.tf
@@ -21,43 +21,43 @@ resource "aws_instance" "test" {
   key_name             = "dan16"
   security_groups      = ["ssh3000"]
   iam_instance_profile = "tfQuotesEC2S3Profile"
-  # user_data            = <<EOF
-  #   #!/bin/bash
-  #   mkdir /var/lib/quotes
-  #   cd /var/lib/quotes
-  #   aws s3 cp s3://dan16-quote-bucket/deployment.tar.gz .
-  #   tar -xvf deployment.tar.gz
-  #   rm deployment.tar.gz
-  #   echo "${file("../common/systemd-quotes.service")}" > /lib/systemd/system/quotes.service
-  #   systemctl daemon-reload
-  #   systemctl enable quotes.service
-  #   systemctl start quotes.service
-  # EOF
+  user_data            = <<EOF
+    #!/bin/bash
+    mkdir /var/lib/quotes
+    cd /var/lib/quotes
+    aws s3 cp s3://dan16-quote-bucket/deployment.tar.gz .
+    tar -xvf deployment.tar.gz
+    rm deployment.tar.gz
+    echo "${file("../common/systemd-quotes.service")}" > /lib/systemd/system/quotes.service
+    systemctl daemon-reload
+    systemctl enable quotes.service
+    systemctl start quotes.service
+  EOF
 
   # provisioner "local-exec" {
   #   command = "nc -v ${self.public_ip} 3000"
   # }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo mkdir /var/lib/quotes",
-      "cd /var/lib/quotes",
-      "sudo aws s3 cp s3://dan16-quote-bucket/deployment.tar.gz .",
-      "sudo tar -xvf deployment.tar.gz",
-      "sudo rm deployment.tar.gz",
-      "echo '${file("../common/systemd-quotes.service")}' | sudo tee /lib/systemd/system/quotes.service",
-      "sudo systemctl daemon-reload",
-      "sudo systemctl enable quotes.service",
-      "sudo systemctl start quotes.service"
-    ]
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "sudo mkdir /var/lib/quotes",
+  #     "cd /var/lib/quotes",
+  #     "sudo aws s3 cp s3://dan16-quote-bucket/deployment.tar.gz .",
+  #     "sudo tar -xvf deployment.tar.gz",
+  #     "sudo rm deployment.tar.gz",
+  #     "echo '${file("../common/systemd-quotes.service")}' | sudo tee /lib/systemd/system/quotes.service",
+  #     "sudo systemctl daemon-reload",
+  #     "sudo systemctl enable quotes.service",
+  #     "sudo systemctl start quotes.service"
+  #   ]
 
-    connection {
-      type        = "ssh"
-      host        = self.public_ip
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      # private_key = var.ssh_private_key
-    }
-  }
+  #   connection {
+  #     type        = "ssh"
+  #     host        = self.public_ip
+  #     user        = "ec2-user"
+  #     # private_key = file("~/.ssh/id_rsa")
+  #     private_key = var.ssh_private_key
+  #   }
+  # }
 
   tags = {
     name = "quotes-test"
