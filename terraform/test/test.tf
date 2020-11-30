a5afb0c007dbc9537abd6a78f28aedc60d45e44c
Derrike <derrike.nunn@gmail.com>
2020-11-30 13:46:56 -0600

    remove quotes db, add decrypt code 2

--- terraform/test/test.tf
+++ terraform/test/test.tf
@@ -14,30 +14,27 @@ variable "GH_SECRET" {
   type = string
 }
 
-data "aws_secretsmanager_secret" "gpg" {
-  name = "quotes/db/pass"
-}
-
-data "aws_secretsmanager_secret_version" "example" {
-  secret_id = data.aws_secretsmanager_secret.gpg.id
-}
-
 resource "aws_instance" "quotes-test" {
   ami                  = "ami-069098bd859abd964"
   instance_type        = "t2.micro"
   key_name             = "dan16"
   security_groups      = ["tfQuotesSGNodeJS3000"]
-  iam_instance_profile = "tfQuotesEC2S3Profile"
+  iam_instance_profile = "tfQuotesEC2Profile"
   # user_data          = <<EOF EOF
 
   provisioner "remote-exec" {
     inline = [
       "sudo mkdir /var/lib/quotes",
       "cd /var/lib/quotes",
+      "sudo yum install jq -y",
       "sudo aws s3 cp s3://dan16-quote-bucket/deployment.tar.gz .",
       "sudo tar -xvf deployment.tar.gz",
       "sudo rm deployment.tar.gz",
-      "echo '${jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["db_encryption_pass"]}${var.GH_SECRET}' > /home/ec2-user/asdf.txt",
+      "sudo aws s3 cp s3://dan16-quote-bucket/quotes_all.csv.gpg ./data/",
+      "cd ./data",
+      "gpg --batch --yes --passphrase \"$${AWS_SECRET}${var.GH_SECRET}\" -o quotes_all.csv -d quotes_all.csv.gpg"
+      "export AWS_SECRET=$(aws secretsmanager get-secret-value --secret-id quotes/db/pass --region us-east-1 --query SecretString --output text | jq -r .db_encryption_pass)",
+      "echo \"$${AWS_SECRET}${var.GH_SECRET}\" > /home/ec2-user/asdf.txt",
       "echo '${file("../common/systemd-quotes.service")}' | sudo tee /etc/systemd/system/quotes.service",
       "sudo systemctl daemon-reload",
       "sudo systemctl enable quotes.service",
@@ -61,10 +58,18 @@ output "instance_ip_addr" {
   value = aws_instance.quotes-test.public_ip
 }
 
-output "tester" {
-  value = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["db_encryption_pass"]
-}
+# data "aws_secretsmanager_secret" "gpg" {
+#   name = "quotes/db/pass"
+# }
 
-output "tester2" {
-  value = var.GH_SECRET
-}
+# data "aws_secretsmanager_secret_version" "example" {
+#   secret_id = data.aws_secretsmanager_secret.gpg.id
+# }
+
+# output "tester" {
+#   value = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["db_encryption_pass"]
+# }
+
+# output "tester2" {
+#   value = var.GH_SECRET
+# }
