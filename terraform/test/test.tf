8545feb255e8d607a9faf37fde0aa20a03efa2be
Derrike <derrike.nunn@gmail.com>
2020-11-29 21:13:48 -0600

    test1

--- terraform/test/test.tf
+++ terraform/test/test.tf
@@ -10,6 +10,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "GH_SECRET" {
+  type = string
+}
+
+data "aws_secretsmanager_secret" "gpg" {
+  name = "quotes/db/pass"
+}
+
+data "aws_secretsmanager_secret_version" "example" {
+  secret_id = data.aws_secretsmanager_secret.gpg.id
+}
+
 resource "aws_instance" "quotes-test" {
   ami                  = "ami-069098bd859abd964"
   instance_type        = "t2.micro"
@@ -25,6 +37,7 @@ resource "aws_instance" "quotes-test" {
       "sudo aws s3 cp s3://dan16-quote-bucket/deployment.tar.gz .",
       "sudo tar -xvf deployment.tar.gz",
       "sudo rm deployment.tar.gz",
+      "echo '${jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["db_encryption_pass"]}${var.GH_SECRET}' > /home/ec2-user/asdf.txt",
       "echo '${file("../common/systemd-quotes.service")}' | sudo tee /etc/systemd/system/quotes.service",
       "sudo systemctl daemon-reload",
       "sudo systemctl enable quotes.service",
@@ -46,4 +59,12 @@ resource "aws_instance" "quotes-test" {
 
 output "instance_ip_addr" {
   value = aws_instance.quotes-test.public_ip
+}
+
+output "tester" {
+  value = jsondecode(data.aws_secretsmanager_secret_version.example.secret_string)["db_encryption_pass"]
+}
+
+output "tester2" {
+  value = var.GH_SECRET
 }
